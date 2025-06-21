511d50b153aecc9a70ac3c2e00ce62d73e5c1176
Hamster <dtaranenko137@gmail.com>
2025-06-21 16:52:46 +0300

    'Done'

--- app/main.py
+++ app/main.py
@@ -1,20 +1,19 @@
 class Person:
     people = {}
-
-    def __init__(self, name: str, age: int) -> None:
+    def __init__(self, name, age):
         self.name = name
         self.age = age
         Person.people[self.name] = self
 
-
 def create_person_list(people: list) -> list:
+    Person.people.clear()
     person_list = []
     for human in people:
         person_list.append(Person(human["name"], human["age"]))
     for human in people:
-        person = Person.people[human["name"]]
+        persona = Person.people[human["name"]]
         if "wife" in human and human["wife"] is not None:
-            person.wife = Person.people[human["wife"]]
+            persona.wife = Person.people[human["wife"]]
         if "husband" in human and human["husband"] is not None:
-            person.husband = Person.people[human["husband"]]
+            persona.husband = Person.people[human["husband"]]
     return person_list
